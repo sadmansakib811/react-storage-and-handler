3df2d67f8e00728ec089eee45680b0ff6975c02b
Sadman Sakib <sadmansakib123450@gmail.com>
2023-03-27 00:32:11 +0200

    set data in localstorage as an object

--- src/utilities/fakedb.js
+++ src/utilities/fakedb.js
@@ -1,14 +1,30 @@
 //use localStorage to manage cart data
 const addToDb = id=>{
-    const quantity = localStorage.getItem(id);
-    if(quantity){
-      const newQuantiy =parseInt (quantity)+1;
-      localStorage.setItem(id, newQuantiy)
+    // const quantity = localStorage.getItem(id);
+    let shoppinCart;
+    const getPrevCartData = localStorage.getItem('shopping-cart');
+    if(getPrevCartData){
+      shoppinCart = JSON.parse(getPrevCartData);
+     
     }
     else{
-        localStorage.setItem(id,1);
+      shoppinCart = {};
     }
-    
+    // add quantity
+    const quantity =shoppinCart[id];
+    if(quantity){
+       const newQuantity = quantity+1;
+       shoppinCart[id] = newQuantity;
+
+      // const newQuantiy =parseInt (quantity)+1;
+      // localStorage.setItem(id, newQuantiy)
+    }
+    else{
+        // localStorage.setItem(id,1);
+        shoppinCart[id] = 1;
+    }
+    localStorage.setItem('shopping-cart',JSON.stringify(shoppinCart));
+
 }
 
 export{addToDb}
